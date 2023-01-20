196eee3545ae95aadce551f1cc8bba5d1edacfc9
Sharona Young <msrona806@gmail.com>
2023-01-20 01:24:27 -0500

    added people array

---
+++ main.js
@@ -0,0 +1,62 @@
+const people = [
+  {
+    "name": "Angela",
+    "age": "64",
+    "occupation": "Actress"
+  },
+  {
+    "name": "Steve",
+    "age": "42",
+    "occupation": "Musician"
+  },
+  {
+    "name": "Will",
+    "age": "50",
+    "occupation": "Comedian"
+  },
+  {
+    "name": "Lisa",
+    "age": "35",
+    "occupation": "Artist"
+  },
+  {
+    "name": "Thomas",
+    "age": "37",
+    "occupation": "Dentist"
+  },
+  {
+    "name": "Brian",
+    "age": "27",
+    "occupation": "Teacher"
+  },
+  {
+    "name": "Megan",
+    "age": "24",
+    "occupation": "Actress"
+  },
+  {
+    "name": "Charles",
+    "age": "23",
+    "occupation": "Developer"
+  },
+  {
+    "name": "Theresa",
+    "age": "42",
+    "occupation": "Secretary"
+  },
+  {
+    "name": "Seth",
+    "age": "38",
+    "occupation": "Writer"
+  },
+  {
+    "name": "Greg",
+    "age": "41",
+    "occupation": "Actor"
+  },
+  {
+    "name": "Olivia",
+    "age": "30",
+    "occupation": "Detective"
+  },
+]
